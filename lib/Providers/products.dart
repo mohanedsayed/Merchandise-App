454977a06da60364ddfef817980c257f11521851
mohaned <mohane.sayed1@gmail.com>
2020-05-03 13:08:54 +0200

    added the cart provider

--- lib/Providers/products.dart
+++ lib/Providers/products.dart
@@ -37,10 +37,28 @@ class Products with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
+//  var _showFavoritesOnly = false;
+
+  List<Product> get favoriteItems {
+    return _items.where((ProductItem) => ProductItem.isFavorite).toList();
+  }
 
   List<Product> get items {
+//    if (_showFavoritesOnly) {
+//      return _items.where((ProductItem) => ProductItem.isFavorite).toList();
+//    }
     return [..._items];
   }
+
+//  void showFavoritesOnly() {
+//    _showFavoritesOnly = true;
+//    notifyListeners();
+//  }
+//
+//  void showAll() {
+//    _showFavoritesOnly = false;
+//    notifyListeners();
+//  }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -2,14 +2,49 @@ import 'package:flutter/material.dart';
 
 import '../widgets/productsGrid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions { Favorites, All }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Shop"),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(
+                () {
+                  if (selectedValue == FilterOptions.Favorites) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                },
+              );
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text("Only favorites"),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text("Only favorites"),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
